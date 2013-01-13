7a5c4877a2a291f423bc3701fdc28c46f52bc46a
guoyao <guoyao.dev@gmail.com>
2013-01-13 19:55:46 +0800

    create a standard rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itshare::Application.config.secret_token = 'ef685e55a922fcda18093fdc70bc9a51bc68b03cd14a80e535bc95c05cdf0183b4eb7903d3b17aafac22b4b4ff68bc60e0245249c717d5606441db3694cacf8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Itshare::Application.config.session_store :cookie_store, key: '_itshare_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Itshare::Application.config.session_store :active_record_store
