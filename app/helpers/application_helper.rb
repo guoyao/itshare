c06dddeac40e4dfa19249eeffa64d8344105ea04
guoyao <guoyao.dev@gmail.com>
2013-01-20 18:05:08 +0800

    layout changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
+# encoding: utf-8
 module ApplicationHelper
+  def created_at_from_now
+    time_from_now(created_at)
+  end
+
+  def updated_at_from_now
+    time_from_now(updated_at)
+  end
+
+  private
+    def time_from_now(time)
+      result = ""
+      now = Time.now
+      seconds = now - time
+      if seconds < 60 then
+        result = "于#{seconds.to_i}秒前"
+      elsif seconds < 3600 then
+        result = "于#{(seconds / 60).to_i}分钟前"
+      elsif seconds < 86400 then  # 3600 * 24
+        result = "于#{(seconds / 3600).to_i}小时前"
+      else
+        result = "于#{(seconds / 86400).to_i}天前"
+      end
+      result
+    end
 end
--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,3 +1,6 @@
 class Experience < ActiveRecord::Base
+  include ApplicationHelper
+
   attr_accessible :company, :details, :title
+
 end
