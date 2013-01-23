7f70d3927256e6ce84960259c6d5660504f37185
guoyao <guoyao.dev@gmail.com>
2013-01-23 10:35:03 +0800

    add experiences to index, add sina weibo meta to index

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,10 @@
 class IndexController < ApplicationController
   def index
+    @experiences = Experience.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @experiences }
+    end
   end
 end
